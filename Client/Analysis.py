72f43c8a9f18a9ac1ea377b4170f02e5d3684be6
Andrew Dailey <andrewdailey0614@gmail.com>
2024-11-29 00:32:20 -0500

    added comments and added download logging to analysis file

---
+++ Client/Analysis.py
@@ -0,0 +1,25 @@
+import datetime
+
+def initializeData(socketTime):
+    # Writes the server response time at the top of the txt file
+    with open('analysis.txt', 'w') as file:
+        file.write(f'--------------------------\nSocket created in {round(socketTime * 10e3, 2)} ms\n--------------------------\n')
+
+def getData(size, time, direction):
+    # Calculates the data transfer speed
+    speed = size / time
+    # Converts the speed to appropriate unit
+    if speed >= 10e9:
+        speed = speed / 10e9
+        unit = "GB/s"
+    elif speed >= 10e6:
+        speed = speed / 10e6
+        unit = "MB/s"
+    elif speed >= 10e3:
+        speed = speed / 10e3
+        unit = "B/s"
+    else:
+        unit = "B/s"
+    # Appends the speed and timestamp to the txt file
+    with open('analysis.txt','a') as fd:
+        fd.write(f'> File {direction} at {datetime.datetime.now()}. Transfer Speed: {round(speed,2)} {unit}\n')
